44e91ff449068cab8a65ff4e92837e86b27024f7
Pedro Henrique <pedroabreu.dev@gmail.com>
2021-08-17 17:12:55 -0400

    08. Refatorando o código

--- app/src/main/java/br/com/diastecnologia/orgs/ui/activity/ListaProdutosActivity.kt
+++ app/src/main/java/br/com/diastecnologia/orgs/ui/activity/ListaProdutosActivity.kt
@@ -3,34 +3,40 @@ package br.com.diastecnologia.orgs.ui.activity
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import br.com.diastecnologia.orgs.R
 import br.com.diastecnologia.orgs.dao.ProdutosDao
-import br.com.diastecnologia.orgs.model.Produto
 import br.com.diastecnologia.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import java.math.BigDecimal
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+class ListaProdutosActivity : AppCompatActivity(R.layout.activity_lista_produtos) {
+
+    private val dao = ProdutosDao()
+    private val adapter = ListaProdutosAdapter(this, produtos = dao.buscaTodos())
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        configuraRecyclerView()
+        configuraFab()
     }
 
     override fun onResume() {
         super.onResume()
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        val dao = ProdutosDao()
-        Log.i("MainActivity", "OnCreate: ${dao.buscaTodos()}")
-        recyclerView.adapter = ListaProdutosAdapter(this, produtos = dao.buscaTodos())
+        adapter.atuliza(dao.buscaTodos())
+    }
 
-        val fab = findViewById<FloatingActionButton>(R.id.floatingActionButton)
+    private fun configuraFab() {
+        val fab = findViewById<FloatingActionButton>(R.id.activity_lista_produto_fab)
         fab.setOnClickListener {
             val intent = Intent(this, FormularioProdutoActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun configuraRecyclerView() {
+        val recyclerView = findViewById<RecyclerView>(R.id.activity_lista_produto_recyclerView)
+        recyclerView.adapter = adapter
     }
 }
