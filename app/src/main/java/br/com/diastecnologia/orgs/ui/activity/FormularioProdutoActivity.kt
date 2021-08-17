9536038b2ef3e5216eda83bc0089ce297dab4079
Pedro Henrique <pedroabreu.dev@gmail.com>
2021-08-17 16:00:03 -0400

    06. Implementando o formulário

--- app/src/main/java/br/com/diastecnologia/orgs/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/br/com/diastecnologia/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -2,10 +2,44 @@ package br.com.diastecnologia.orgs.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import br.com.diastecnologia.orgs.R
+import br.com.diastecnologia.orgs.model.Produto
+import java.math.BigDecimal
 
 class FormularioProdutoActivity :
     AppCompatActivity(R.layout.activity_formulario_produto) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val botaoSalvar = findViewById<Button>(R.id.botao_salvar)
+        botaoSalvar.setOnClickListener {
+            val campoNome = findViewById<EditText>(R.id.nome)
+            val nome = campoNome.text.toString()
+            val campoDescricao = findViewById<EditText>(R.id.descricao)
+            val descricao = campoDescricao.text.toString()
+            val campoValor = findViewById<EditText>(R.id.valor)
+            val valorEmTexto = campoValor.text.toString()
+            val valor = if(valorEmTexto.isBlank()){
+                BigDecimal.ZERO
+            }else{
+                BigDecimal(valorEmTexto)
+            }
+
+            val produtoNovo = Produto(
+                nome = nome,
+                descricao = descricao,
+                valor = valor
+            )
+
+            Log.i("FormularioProduto", "onCreate: $produtoNovo")
+        }
+    }
 
 
 }
